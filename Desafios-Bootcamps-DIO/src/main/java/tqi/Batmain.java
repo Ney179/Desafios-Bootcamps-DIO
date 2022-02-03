8db3c58f63c2f78bb8c090c1a34d686c829baa9d
Ney179 <84090794+Ney179@users.noreply.github.com>
2022-02-03 20:41:14 -0300

    Add files via upload
    Adicionando o pacote TQI e os desafios

---
+++ Desafios-Bootcamps-DIO/src/main/java/tqi/Batmain.java
@@ -0,0 +1,20 @@
+package tqi;
+
+import java.util.Scanner;
+
+public class Batmain {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int T = scan.nextInt();
+
+        int viloesCapturados = 0;
+        while(viloesCapturados < T) {
+            String vilao = scan.next();
+            //TODO: Imprima "Y" na linha abaixo, pois já temos o vilão em mãos ;-)
+            System.out.println("Y");
+            viloesCapturados++;
+        }
+    }
+}
